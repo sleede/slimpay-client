26a0ac05fe696c6a2e2524781541135daba7ce3c
Nicolas Florentin <nicolas@sleede.com>
2019-07-11 15:19:41 +0200

    adds update in test for method_name

--- lib/slimpay.rb
+++ lib/slimpay.rb
@@ -73,7 +73,7 @@ module Slimpay
           http_method = :get
 
           # HACK: don't have the information from the API
-          if method_name.start_with?('create', 'post', 'revoke', 'cancel', 'ack')
+          if method_name.start_with?('create', 'post', 'revoke', 'cancel', 'ack', 'update')
             http_method = :post
           elsif method_name.start_with?('patch')
             http_method = :patch
